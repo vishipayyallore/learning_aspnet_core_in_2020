4c32bbddffacaed620d120afe57c1f3fcf3e65b3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-04-17 16:40:34 +0530

    Few Chagnes

--- First.DotNetCore.WebAppWith.Angular9/SampleProducts/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ First.DotNetCore.WebAppWith.Angular9/SampleProducts/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,5 +1,15 @@
 import { Component, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+// const baseApiUrl = 'https://weather-forecast-service.azurewebsites.net/api/';  // Direct Web Api
+// const baseApiUrl = 'https://weather-forecast-apimgw.azure-api.net/wfsapimgw/api/'; // Using API Mgt
+const baseApiUrl = 'https://college-service-apimgw.azure-api.net/wfsapimgw/api/';
+
+// With Authorization
+const headersOptions = new HttpHeaders({
+  'Ocp-Apim-Subscription-Key': 'YourKey'
+});
+
 
 @Component({
   selector: 'app-fetch-data',
@@ -8,12 +18,15 @@ import { HttpClient } from '@angular/common/http';
 export class FetchDataComponent {
   public forecasts: WeatherForecast[];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
+  constructor(http: HttpClient) {
+    http.get<WeatherForecast[]>(`${baseApiUrl}weatherforecast`, { headers: headersOptions }).subscribe(result => {
       this.forecasts = result;
     }, error => console.error(error));
   }
+
 }
+
+// 
 
 interface WeatherForecast {
   date: string;
@@ -21,3 +34,15 @@ interface WeatherForecast {
   temperatureF: number;
   summary: string;
 }
+
+
+/*
+constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+  let baseApiUrl = 'https://weather-forecast-service.azurewebsites.net/api/';
+  http.get<WeatherForecast[]>( `${baseApiUrl}weatherforecast`).subscribe(result => {
+    this.forecasts = result;
+  }, error => console.error(error));
+}
+*/
+
+
--- First.DotNetCore.WebAppWith.Angular9/SampleProducts/ClientApp/src/main.ts
+++ First.DotNetCore.WebAppWith.Angular9/SampleProducts/ClientApp/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 export function getBaseUrl() {
+  // alert(`Href: ${document.getElementsByTagName('base')[0].href}`);
   return document.getElementsByTagName('base')[0].href;
 }
 
