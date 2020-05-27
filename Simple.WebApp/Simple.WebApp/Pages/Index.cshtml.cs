0cfd8781be656510cdb60ea5d1e42006ff44c16f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-05-27 15:49:55 +0530

    Initila Version

---
+++ Simple.WebApp/Simple.WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Simple.WebApp.Pages
+{
+
+    public enum IsolationType
+    {
+        Mechanical = 1,
+        Electrical = 2
+    }
+
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
